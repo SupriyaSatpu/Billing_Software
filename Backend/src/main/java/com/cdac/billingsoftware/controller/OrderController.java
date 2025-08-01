feeac99183d86f763044e912336054f53bd8b5f4
Supriya Satpute <satpute.supriya17@gmail.com>
2025-08-02 01:20:37 +0530

    Added Order module (entity, repository, service, controller, and DTOs)

---
+++ Backend/src/main/java/com/cdac/billingsoftware/controller/OrderController.java
@@ -0,0 +1,34 @@
+package com.cdac.billingsoftware.controller;
+
+import com.cdac.billingsoftware.io.OrderRequest;
+import com.cdac.billingsoftware.io.OrderResponse;
+import com.cdac.billingsoftware.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponse createOrder(@RequestBody OrderRequest request) {
+        return orderService.createOrder(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{orderId}")
+    public void deleteOrder(@PathVariable String orderId) {
+        orderService.deleteOrder(orderId);
+    }
+
+    @GetMapping("/latest")
+    public List<OrderResponse> getLatestOrders() {
+        return orderService.getLatestOrders();
+    }
+}
---
+++ Backend/src/main/java/com/cdac/billingsoftware/service/OrderService.java
@@ -0,0 +1,25 @@
+package com.cdac.billingsoftware.service;
+
+import com.cdac.billingsoftware.io.OrderRequest;
+import com.cdac.billingsoftware.io.OrderResponse;
+import com.cdac.billingsoftware.io.PaymentVerificationRequest;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface OrderService {
+
+    OrderResponse createOrder(OrderRequest request);
+
+    void deleteOrder(String orderId);
+
+    List<OrderResponse> getLatestOrders();
+
+    OrderResponse verifyPayment(PaymentVerificationRequest request);
+
+    //Double sumSalesByDate(LocalDate date);
+
+    //Long countByOrderDate(LocalDate date);
+
+    //List<OrderResponse> findRecentOrders();
+}
