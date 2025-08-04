3b80395a7d6dd6af4426465a2a1abad667737f31
Supriya Satpute <satpute.supriya17@gmail.com>
2025-08-04 19:13:23 +0530

    Added Manage Category UI with form, list, and search functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,15 @@ const App = () => {
     return (
         <div>
             <Menubar />
+            <Routes>
+                <Route path = "/dashboard" element={<Dashboard/>}/>
+                <Route path = "/category" element={<ManageCategory/>}/>
+                <Route path = "/users" element={<ManageUsers/>}/>
+                <Route path = "/items" element={<ManageItems/>}/>
+                <Route path = "/explore" element={<Explore/>}/>
+                <Route path = "/" element={<Dashboard/>}/>
+
+            </Routes>
         </div>
     );
 }
